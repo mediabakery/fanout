f2b17cc03efef78553b52e79c6aea0579832bc27
Sebastian Tilch <mediabakery@users.noreply.github.com>
2024-06-22 23:08:29 +0200

    feat: :zap: init

---
+++ cmd/receiver/main.go
@@ -0,0 +1,126 @@
+package main
+
+import (
+	"bytes"
+	"context"
+	"crypto/aes"
+	"crypto/cipher"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/nats-io/nats.go"
+	"github.com/nats-io/nats.go/jetstream"
+)
+
+var defaultTimeout = time.Second * 30
+
+func decrypt(key []byte, encrypted []byte) ([]byte, error) {
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+	aesgcm, err := cipher.NewGCM(block)
+	if err != nil {
+		return nil, err
+	}
+	return aesgcm.Open(nil, encrypted[:12], encrypted[12:], nil)
+}
+
+func send(ctx context.Context, method string, targetURL string, body []byte) error {
+	req, err := http.NewRequestWithContext(ctx, method, targetURL, bytes.NewReader(body))
+	if err != nil {
+		return fmt.Errorf("failed to create request: %s - %v", targetURL, err)
+	}
+
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+
+	client := &http.Client{
+		Timeout: defaultTimeout,
+	}
+	resp, err := client.Do(req)
+	if err != nil {
+		return fmt.Errorf("failed to reach target: %s - %v", targetURL, err)
+	}
+
+	defer resp.Body.Close()
+	return nil
+}
+
+func mustGetEnv(key, reason string) string {
+	value := os.Getenv(key)
+	if value == "" {
+		log.Fatalf("cannot get %s via \"%s\" from environment", reason, key)
+	}
+	return value
+}
+
+func main() {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	targetURL := mustGetEnv("TARGET_URL", "target url")
+	key := []byte(mustGetEnv("KEY", "end2end encryption key"))
+	natsURL := mustGetEnv("NATS_URL", "nats url")
+	jsName := mustGetEnv("JS_NAME", "jetstream name")
+	subject := mustGetEnv("SUBJECT", "nats subject")
+	clientName := mustGetEnv("CLIENT_NAME", "client name")
+	natsCredentials := mustGetEnv("NATS_CREDENTIALS", "path to nats credentials")
+
+	nc, err := nats.Connect(natsURL, nats.Name(clientName), nats.UserCredentials(natsCredentials), nats.MaxReconnects(-1))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer nc.Close()
+	js, err := jetstream.New(nc)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	s, err := js.CreateOrUpdateStream(ctx, jetstream.StreamConfig{
+		Name:     jsName,
+		Subjects: []string{subject},
+		MaxBytes: 1 * 1024 * 1024,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	c, err := s.CreateOrUpdateConsumer(ctx, jetstream.ConsumerConfig{
+		Durable:   clientName,
+		AckPolicy: jetstream.AckExplicitPolicy,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println(1)
+	cons, err := c.Consume(func(msg jetstream.Msg) {
+		log.Printf("Received a JetStream message on subject %s", msg.Subject())
+
+		body, err := decrypt(key, msg.Data())
+		if err != nil {
+			log.Printf("Error decrypting: %v", err)
+			msg.Nak()
+			return
+		}
+		err = send(ctx, http.MethodPost, targetURL, body)
+		if err != nil {
+			log.Printf("Error sending: %v for message", err)
+			msg.Nak()
+			return
+		}
+		msg.Ack()
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer cons.Stop()
+	log.Println(3)
+	done := make(chan os.Signal, 1)
+	signal.Notify(done, syscall.SIGINT, syscall.SIGTERM)
+	fmt.Println("Blocking, press ctrl+c to continue...")
+	<-done
+
+}
---
+++ cmd/sender/main.go
@@ -0,0 +1,143 @@
+package main
+
+import (
+	"bytes"
+	"context"
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/nats-io/nats.go"
+	"github.com/nats-io/nats.go/jetstream"
+)
+
+var defaultTimeout = time.Second * 30
+
+func encrypt(key []byte, message []byte) ([]byte, error) {
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+	nonce := make([]byte, 12)
+	if _, err := io.ReadFull(rand.Reader, nonce); err != nil {
+		return nil, err
+	}
+
+	aesgcm, err := cipher.NewGCM(block)
+	if err != nil {
+		return nil, err
+	}
+	return append(nonce, aesgcm.Seal(nil, nonce, message, nil)...), nil
+}
+
+func publish(ctx context.Context, subject string, body []byte, js jetstream.JetStream, key []byte) (*jetstream.PubAck, error) {
+	enc, err := encrypt(key, body)
+	if err != nil {
+		return nil, fmt.Errorf("can't encrypt: %v", err)
+	}
+	ack, err := js.Publish(ctx, subject, enc)
+	if err != nil {
+		return nil, fmt.Errorf("can't publish: %v", err)
+	}
+	return ack, nil
+}
+
+func send(ctx context.Context, targetURL string, body []byte, w http.ResponseWriter, r *http.Request) error {
+	proxyReq, err := http.NewRequestWithContext(ctx, r.Method, targetURL, bytes.NewReader(body))
+	if err != nil {
+		http.Error(w, "Failed to create request to target", http.StatusBadRequest)
+		return fmt.Errorf("Failed to create request: %s - %v", targetURL, err)
+	}
+
+	proxyReq.Header = r.Header.Clone()
+
+	client := &http.Client{
+		Timeout: defaultTimeout,
+	}
+	resp, err := client.Do(proxyReq)
+	if err != nil {
+		http.Error(w, "Failed to reach target", http.StatusNotFound)
+		return fmt.Errorf("Failed to reach target: %s - %v", targetURL, err)
+	}
+	defer resp.Body.Close()
+
+	for header, values := range resp.Header {
+		for _, value := range values {
+			w.Header().Add(header, value)
+		}
+	}
+
+	w.WriteHeader(resp.StatusCode)
+	_, err = io.Copy(w, resp.Body)
+	if err != nil {
+		http.Error(w, "Failed to copy response body", http.StatusInternalServerError)
+		return fmt.Errorf("Failed to copy response body: %s - %v", targetURL, err)
+	}
+	return nil
+}
+
+func mustGetEnv(key, reason string) string {
+	value := os.Getenv(key)
+	if value == "" {
+		log.Fatalf("cannot get %s via \"%s\" from environment", reason, key)
+	}
+	return value
+}
+
+func main() {
+	ctx := context.Background()
+
+	addr := mustGetEnv("ADDR", "server address to listen on")
+	targetURL := mustGetEnv("TARGET_URL", "target url")
+	key := []byte(mustGetEnv("KEY", "end2end encryption key"))
+	natsURL := mustGetEnv("NATS_URL", "nats url")
+	subject := mustGetEnv("SUBJECT", "nats subject")
+	clientName := mustGetEnv("CLIENT_NAME", "client name")
+	natsCredentials := mustGetEnv("NATS_CREDENTIALS", "path to nats credentials")
+
+	nc, err := nats.Connect(natsURL, nats.Name(clientName), nats.UserCredentials(natsCredentials), nats.MaxReconnects(-1))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer nc.Close()
+	js, err := jetstream.New(nc)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		body, err := io.ReadAll(r.Body)
+		if err != nil {
+			log.Printf("Error reading body: %v", err)
+			http.Error(w, "can't read body", http.StatusBadRequest)
+			return
+		}
+
+		go func(ctx context.Context, subject string, body []byte, js jetstream.JetStream) {
+			ack, err := publish(ctx, subject, body, js, key)
+			if err != nil {
+				log.Printf("Error publishing: %v", err)
+			} else {
+				log.Printf("Published to %s @ %s - Sequence: %d", ack.Domain, ack.Stream, ack.Sequence)
+			}
+		}(ctx, subject, body, js)
+		err = send(ctx, targetURL, body, w, r)
+		if err != nil {
+			log.Printf("Error sending: %v", err)
+		}
+
+	})
+
+	log.Println("Listening on", addr)
+	err = http.ListenAndServe(addr, mux)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
